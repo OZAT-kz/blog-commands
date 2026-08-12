57182e5ba0b7252cc7156a35818cc424d2c70e39
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-12 11:58:26 +0500

    Add/Update: gcloud-compute-security-policies-create.sh

---
+++ gcloud-compute-security-policies-create.sh
@@ -0,0 +1,11 @@
+# ==============================================================================
+# Blocking DDoS traffic via Cloud Armor
+# Source: OZAT Engineering Blog (https://ozat.kz)
+# GitHub: https://github.com/OZAT-kz/blog-commands/blob/main/gcloud-compute-security-policies-create.sh
+# ==============================================================================
+
+
+gcloud compute security-policies rules create 1000 \
+    --security-policy=prod-armor \
+    --src-ip-ranges="45.22.12.0/24" \
+    --action=deny-403
