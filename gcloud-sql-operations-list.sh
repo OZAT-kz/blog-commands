f515a1697bccc596d48ef6b64115313dd84afc72
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-12 11:35:20 +0500

    Add/Update: gcloud-sql-operations-list.sh

---
+++ gcloud-sql-operations-list.sh
@@ -0,0 +1,10 @@
+
+-- Ұсыныстарды алу
+CREATE OR REPLACE TABLE \`ecommerce_ml.user_recommendations\` AS
+SELECT
+  user_id,
+  ARRAY_AGG(STRUCT(item_id, predicted_interaction_score) ORDER BY predicted_interaction_score DESC LIMIT 5) AS recommended_items
+FROM
+  ML.RECOMMEND(MODEL \`ecommerce_ml.item_recommender\`)
+GROUP BY
+  user_id;
