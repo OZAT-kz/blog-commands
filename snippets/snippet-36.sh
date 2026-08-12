f1e3b76706192f05944451ccd474fd86b9e99365
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-12 11:57:25 +0500

    Cleanup old snippet-36.sh

--- snippets/snippet-36.sh
+++
@@ -1,5 +0,0 @@
-# ==============================================================================
-# Provided by OZAT (https://github.com/OZAT-kz)
-# ==============================================================================
-
-gcloud run services update api-gateway --max-instances=200 --region=europe-west3
