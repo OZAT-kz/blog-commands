81eed43d7dd56df87ed032f864798fcb5663f95d
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-12 11:58:27 +0500

    Add/Update: gcloud-projects-add-iam-policy-binding.sh

---
+++ gcloud-projects-add-iam-policy-binding.sh
@@ -0,0 +1,10 @@
+# ==============================================================================
+# Granting temporary IAM roles
+# Source: OZAT Engineering Blog (https://ozat.kz)
+# GitHub: https://github.com/OZAT-kz/blog-commands/blob/main/gcloud-projects-add-iam-policy-binding.sh
+# ==============================================================================
+
+
+gcloud projects add-iam-policy-binding my-ecommerce-prod-kz \
+    --member="user:lead.dev@ozat.kz" \
+    --role="roles/cloudsql.admin"
