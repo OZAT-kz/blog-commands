e7cb63eda56cd85ce17d40ae30283c368712ee71
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-12 11:57:16 +0500

    Cleanup old snippet-12.sh

--- snippets/snippet-12.sh
+++
@@ -1,8 +0,0 @@
-# ==============================================================================
-# Provided by OZAT (https://github.com/OZAT-kz)
-# ==============================================================================
-
-gcloud compute security-policies rules create 1000 \
-    --security-policy=prod-armor \
-    --src-ip-ranges="45.22.12.0/24" \
-    --action=deny-403
