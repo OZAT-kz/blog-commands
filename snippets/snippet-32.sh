e37ed951e9961f4847783d8c29fcae71a2543006
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-12 11:57:21 +0500

    Cleanup old snippet-32.sh

--- snippets/snippet-32.sh
+++
@@ -1,5 +0,0 @@
-# ==============================================================================
-# Provided by OZAT (https://github.com/OZAT-kz)
-# ==============================================================================
-
-gcloud logging read 'resource.type="cloud_run_revision" AND severity>=ERROR' --limit=5 --format=json
