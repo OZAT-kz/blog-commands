7b949e806a41250132969807d66d3552ff25a0dc
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-12 11:57:37 +0500

    Cleanup old snippet-7.sh

--- snippets/snippet-7.sh
+++
@@ -1,5 +0,0 @@
-# ==============================================================================
-# Provided by OZAT (https://github.com/OZAT-kz)
-# ==============================================================================
-
-gcloud container clusters get-credentials prod-cluster --region=europe-west3
