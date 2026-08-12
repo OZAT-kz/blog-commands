8e1ad39bf19a88e61d987ae6a79290f348332824
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-12 11:58:11 +0500

    Add/Update: gcloud-config-set-project.sh

--- gcloud-config-set-project.sh
+++ gcloud-config-set-project.sh
@@ -1,31 +1,8 @@
+# ==============================================================================
+# Setting the active Google Cloud project
+# Source: OZAT Engineering Blog (https://ozat.kz)
+# GitHub: https://github.com/OZAT-kz/blog-commands/blob/main/gcloud-config-set-project.sh
+# ==============================================================================
 
--- Оқыту үшін деректер витринасын құру
-CREATE OR REPLACE TABLE \`ecommerce_ml.training_data\` AS
-WITH user_item_interactions AS (
-  SELECT
-    user_pseudo_id AS user_id,
-    (SELECT value.string_value FROM UNNEST(items) LIMIT 1) AS item_id,
-    event_name
-  FROM
-    \`your-project.analytics_12345.events_*\`
-  WHERE
-    event_name IN ('view_item', 'add_to_cart', 'purchase')
-    AND _TABLE_SUFFIX BETWEEN FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE(), INTERVAL 90 DAY)) AND FORMAT_DATE('%Y%m%d', CURRENT_DATE())
-)
-SELECT
-  user_id,
-  item_id,
-  SUM(
-    CASE 
-      WHEN event_name = 'view_item' THEN 1
-      WHEN event_name = 'add_to_cart' THEN 3
-      WHEN event_name = 'purchase' THEN 10
-      ELSE 0
-    END
-  ) AS interaction_score
-FROM
-  user_item_interactions
-WHERE item_id IS NOT NULL
-GROUP BY
-  user_id, item_id
-HAVING interaction_score >= 1;
+
+gcloud config set project my-ecommerce-prod-kz
