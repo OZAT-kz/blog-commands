e15f14b4404c9e3151cb35e3edd65553cd651c31
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-12 11:58:18 +0500

    Add/Update: gcloud-container-clusters-get-credentials.sh

--- gcloud-container-clusters-get-credentials.sh
+++ gcloud-container-clusters-get-credentials.sh
@@ -1,18 +1,8 @@
+# ==============================================================================
+# Authenticating with GKE clusters
+# Source: OZAT Engineering Blog (https://ozat.kz)
+# GitHub: https://github.com/OZAT-kz/blog-commands/blob/main/gcloud-container-clusters-get-credentials.sh
+# ==============================================================================
 
--- Обучение модели матричной факторизации
-CREATE OR REPLACE MODEL \`ecommerce_ml.item_recommender\`
-OPTIONS(
-  model_type='matrix_factorization',
-  user_col='user_id',
-  item_col='item_id',
-  rating_col='interaction_score',
-  feedback_type='implicit', -- У нас неявные оценки (не 5 звезд, а клики)
-  l2_reg=0.1,               -- Регуляризация, чтобы не переобучиться на ботах
-  num_factors=20            -- Размерность скрытых векторов (эмбеддингов)
-) AS
-SELECT
-  user_id,
-  item_id,
-  interaction_score
-FROM
-  \`ecommerce_ml.training_data\`;
+
+gcloud container clusters get-credentials prod-cluster --region=europe-west3
