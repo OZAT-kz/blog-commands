ca9ffb096892284482b69828fdbf3b1ad00763bd
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-12 11:58:17 +0500

    Add/Update: gcloud-run-services-update-scale.sh

--- gcloud-run-services-update-scale.sh
+++ gcloud-run-services-update-scale.sh
@@ -1,31 +1,8 @@
+# ==============================================================================
+# Scaling Cloud Run max instances
+# Source: OZAT Engineering Blog (https://ozat.kz)
+# GitHub: https://github.com/OZAT-kz/blog-commands/blob/main/gcloud-run-services-update-scale.sh
+# ==============================================================================
 
--- Создаем витрину данных для обучения
-CREATE OR REPLACE TABLE \`ecommerce_ml.training_data\` AS
-WITH user_item_interactions AS (
-  SELECT
-    user_pseudo_id AS user_id,
-    (SELECT value.string_value FROM UNNEST(items) LIMIT 1) AS item_id,
-    event_name
-  FROM
-    \`your-project.analytics_12345.events_*\`
-  WHERE
-    event_name IN ('view_item', 'add_to_cart', 'purchase')
-    AND _TABLE_SUFFIX BETWEEN FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE(), INTERVAL 90 DAY)) AND FORMAT_DATE('%Y%m%d', CURRENT_DATE())
-)
-SELECT
-  user_id,
-  item_id,
-  SUM(
-    CASE 
-      WHEN event_name = 'view_item' THEN 1
-      WHEN event_name = 'add_to_cart' THEN 3
-      WHEN event_name = 'purchase' THEN 10
-      ELSE 0
-    END
-  ) AS interaction_score
-FROM
-  user_item_interactions
-WHERE item_id IS NOT NULL
-GROUP BY
-  user_id, item_id
-HAVING interaction_score >= 1;
+
+gcloud run services update api-gateway --max-instances=200 --region=europe-west3
