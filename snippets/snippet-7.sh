a8759db35d1c63f9a19845baa3827ac684023a8d
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-12 11:13:31 +0500

    Auto-sync blog snippet: snippets/snippet-7.sh

---
+++ snippets/snippet-7.sh
@@ -0,0 +1,5 @@
+# ==============================================================================
+# Provided by OZAT (https://github.com/OZAT-kz)
+# ==============================================================================
+
+gcloud container clusters get-credentials prod-cluster --region=europe-west3
