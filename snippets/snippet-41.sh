5a06479b522652835fd7e8b885bdc02372848228
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-12 11:14:16 +0500

    Auto-sync blog snippet: snippets/snippet-41.sh

---
+++ snippets/snippet-41.sh
@@ -0,0 +1,5 @@
+# ==============================================================================
+# Provided by OZAT (https://github.com/OZAT-kz)
+# ==============================================================================
+
+gcloud run services update-traffic api-gateway --to-revisions=api-gateway-00042-v1x=100 --region=europe-west3
