b89ec2f90630c1890c40f75da106bf537ffcc2f0
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-12 11:57:18 +0500

    Cleanup old snippet-14.sh

--- snippets/snippet-14.sh
+++
@@ -1,5 +0,0 @@
-# ==============================================================================
-# Provided by OZAT (https://github.com/OZAT-kz)
-# ==============================================================================
-
-echo -n "sk_live_new_sms_key_123456" | gcloud secrets versions add sms-api-key --data-file=-
