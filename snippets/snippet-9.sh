8e27bf67e65d2ed25ae86892ca50437e13c7d4a1
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-12 11:13:34 +0500

    Auto-sync blog snippet: snippets/snippet-9.sh

---
+++ snippets/snippet-9.sh
@@ -0,0 +1,5 @@
+# ==============================================================================
+# Provided by OZAT (https://github.com/OZAT-kz)
+# ==============================================================================
+
+kubectl rollout restart deployment/email-worker-deployment -n background-workers
