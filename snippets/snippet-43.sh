dd3f76369601b95b70e3a6ad1bf5c08709b4075c
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-12 11:57:32 +0500

    Cleanup old snippet-43.sh

--- snippets/snippet-43.sh
+++
@@ -1,7 +0,0 @@
-# ==============================================================================
-# Provided by OZAT (https://github.com/OZAT-kz)
-# ==============================================================================
-
-gcloud projects add-iam-policy-binding my-ecommerce-prod-kz \
-    --member="user:lead.dev@ozat.kz" \
-    --role="roles/cloudsql.admin"
