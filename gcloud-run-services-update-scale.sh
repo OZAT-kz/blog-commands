ebbc6f979623df11d3e702dbbe672008f21c11b7
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-26 13:45:18 +0500

    Update code snippet: gcloud-run-services-update-scale.sh

--- gcloud-run-services-update-scale.sh
+++ gcloud-run-services-update-scale.sh
@@ -1,5 +1,5 @@
 # ==============================================================================
-# Scaling Cloud Run max instances
+# gcloud-run-services-update-scale.sh
 # Source: OZAT Engineering Blog (https://ozat.kz)
 # GitHub: https://github.com/OZAT-kz/blog-commands/blob/main/gcloud-run-services-update-scale.sh
 # ==============================================================================
