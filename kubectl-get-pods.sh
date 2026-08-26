4ede1f7cbb3c6fe335ccb682a158a99cfd4e7c79
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-26 13:45:21 +0500

    Update code snippet: kubectl-get-pods.sh

--- kubectl-get-pods.sh
+++ kubectl-get-pods.sh
@@ -1,5 +1,5 @@
 # ==============================================================================
-# Listing pods in a specific Kubernetes namespace
+# kubectl-get-pods.sh
 # Source: OZAT Engineering Blog (https://ozat.kz)
 # GitHub: https://github.com/OZAT-kz/blog-commands/blob/main/kubectl-get-pods.sh
 # ==============================================================================
