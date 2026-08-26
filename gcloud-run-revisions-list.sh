be0dbbad476334e53d66ff4cb23d92acc03dd0da
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-26 13:45:24 +0500

    Update code snippet: gcloud-run-revisions-list.sh

--- gcloud-run-revisions-list.sh
+++ gcloud-run-revisions-list.sh
@@ -1,5 +1,5 @@
 # ==============================================================================
-# Listing Cloud Run revisions
+# gcloud-run-revisions-list.sh
 # Source: OZAT Engineering Blog (https://ozat.kz)
 # GitHub: https://github.com/OZAT-kz/blog-commands/blob/main/gcloud-run-revisions-list.sh
 # ==============================================================================
