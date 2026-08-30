9b0cc938b4e2be819bbf64eb74a1571f1193b458
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-30 08:33:56 +0500

    Update header to OZAT Engineering Hub: gcloud-run-services-update-traffic.sh

--- gcloud-run-services-update-traffic.sh
+++ gcloud-run-services-update-traffic.sh
@@ -1,6 +1,6 @@
 # ==============================================================================
 # gcloud-run-services-update-traffic.sh
-# Source: OZAT Engineering Blog (https://ozat.kz)
+# Source: OZAT Engineering Hub (https://ozat.kz)
 # GitHub: https://github.com/OZAT-kz/blog-commands/blob/main/gcloud-run-services-update-traffic.sh
 # ==============================================================================
 
