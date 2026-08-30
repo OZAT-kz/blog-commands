35ceee833ce46a233133af55aa160d7a15f66421
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-30 08:38:52 +0500

    Update header to OZAT Engineering Hub: kubectl-rollout-restart.sh

--- kubectl-rollout-restart.sh
+++ kubectl-rollout-restart.sh
@@ -1,8 +1,7 @@
 # ==============================================================================
-# kubectl-rollout-restart.sh
+# Спас прод из поезда "Тальго" Алматы-Астана: 10 команд Cloud Shell, которые спасут ваш DevOps без ноутбука
 # Source: OZAT Engineering Hub (https://ozat.kz)
 # GitHub: https://github.com/OZAT-kz/blog-commands/blob/main/kubectl-rollout-restart.sh
 # ==============================================================================
-
 
 kubectl rollout restart deployment/email-worker-deployment -n background-workers
