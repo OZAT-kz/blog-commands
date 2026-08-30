96b0daf83b3289a3ed5f76dde9f950ab75bbce2b
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-30 08:38:44 +0500

    Update header to OZAT Engineering Hub: gcloud-sql-instances-describe.sh

--- gcloud-sql-instances-describe.sh
+++ gcloud-sql-instances-describe.sh
@@ -1,8 +1,7 @@
 # ==============================================================================
-# gcloud-sql-instances-describe.sh
+# Спас прод из поезда "Тальго" Алматы-Астана: 10 команд Cloud Shell, которые спасут ваш DevOps без ноутбука
 # Source: OZAT Engineering Hub (https://ozat.kz)
 # GitHub: https://github.com/OZAT-kz/blog-commands/blob/main/gcloud-sql-instances-describe.sh
 # ==============================================================================
-
 
 gcloud sql instances list --filter="name=prod-pg-db"
