755f617e5c8cdd6702e26ecd24ddad54b7f7a0b3
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-30 08:38:43 +0500

    Update header to OZAT Engineering Hub: gcloud-run-logs-tail.sh

--- gcloud-run-logs-tail.sh
+++ gcloud-run-logs-tail.sh
@@ -1,8 +1,7 @@
 # ==============================================================================
-# gcloud-run-logs-tail.sh
+# Спас прод из поезда "Тальго" Алматы-Астана: 10 команд Cloud Shell, которые спасут ваш DevOps без ноутбука
 # Source: OZAT Engineering Hub (https://ozat.kz)
 # GitHub: https://github.com/OZAT-kz/blog-commands/blob/main/gcloud-run-logs-tail.sh
 # ==============================================================================
-
 
 gcloud logging read 'resource.type="cloud_run_revision" AND severity>=ERROR' --limit=5 --format=json
