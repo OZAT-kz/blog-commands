8cc58e11feadcb6ee869d15fc13128b795d151a4
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-30 08:38:45 +0500

    Update header to OZAT Engineering Hub: gcloud-sql-operations-list.sh

--- gcloud-sql-operations-list.sh
+++ gcloud-sql-operations-list.sh
@@ -1,8 +1,7 @@
 # ==============================================================================
-# gcloud-sql-operations-list.sh
+# Спас прод из поезда "Тальго" Алматы-Астана: 10 команд Cloud Shell, которые спасут ваш DevOps без ноутбука
 # Source: OZAT Engineering Hub (https://ozat.kz)
 # GitHub: https://github.com/OZAT-kz/blog-commands/blob/main/gcloud-sql-operations-list.sh
 # ==============================================================================
-
 
 gcloud sql operations list --instance=prod-pg-db --limit=3
