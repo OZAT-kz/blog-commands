1f090d0da766a1d00e61e1989cb4759ebabc3456
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-30 08:38:59 +0500

    Update header to OZAT Engineering Hub: gcloud-projects-add-iam-policy-binding.sh

--- gcloud-projects-add-iam-policy-binding.sh
+++ gcloud-projects-add-iam-policy-binding.sh
@@ -1,9 +1,8 @@
 # ==============================================================================
-# gcloud-projects-add-iam-policy-binding.sh
+# Спас прод из поезда "Тальго" Алматы-Астана: 10 команд Cloud Shell, которые спасут ваш DevOps без ноутбука
 # Source: OZAT Engineering Hub (https://ozat.kz)
 # GitHub: https://github.com/OZAT-kz/blog-commands/blob/main/gcloud-projects-add-iam-policy-binding.sh
 # ==============================================================================
-
 
 gcloud projects add-iam-policy-binding my-ecommerce-prod-kz \
     --member="user:lead.dev@ozat.kz" \
