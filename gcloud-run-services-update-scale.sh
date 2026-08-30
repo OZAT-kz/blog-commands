53fe2d656aa9fa01c4ba1e9983129cd917a9dc35
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-30 08:38:48 +0500

    Update header to OZAT Engineering Hub: gcloud-run-services-update-scale.sh

--- gcloud-run-services-update-scale.sh
+++ gcloud-run-services-update-scale.sh
@@ -1,8 +1,7 @@
 # ==============================================================================
-# gcloud-run-services-update-scale.sh
+# Спас прод из поезда "Тальго" Алматы-Астана: 10 команд Cloud Shell, которые спасут ваш DevOps без ноутбука
 # Source: OZAT Engineering Hub (https://ozat.kz)
 # GitHub: https://github.com/OZAT-kz/blog-commands/blob/main/gcloud-run-services-update-scale.sh
 # ==============================================================================
-
 
 gcloud run services update api-gateway --max-instances=200 --region=europe-west3
