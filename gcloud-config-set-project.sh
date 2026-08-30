1e2cdf71f345c969ed86585822ecbfdf27889f6d
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-30 08:38:41 +0500

    Update header to OZAT Engineering Hub: gcloud-config-set-project.sh

--- gcloud-config-set-project.sh
+++ gcloud-config-set-project.sh
@@ -1,8 +1,7 @@
 # ==============================================================================
-# gcloud-config-set-project.sh
+# Спас прод из поезда "Тальго" Алматы-Астана: 10 команд Cloud Shell, которые спасут ваш DevOps без ноутбука
 # Source: OZAT Engineering Hub (https://ozat.kz)
 # GitHub: https://github.com/OZAT-kz/blog-commands/blob/main/gcloud-config-set-project.sh
 # ==============================================================================
-
 
 gcloud config set project my-ecommerce-prod-kz
