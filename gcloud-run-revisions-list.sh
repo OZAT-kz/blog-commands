5175a60e2fb09882ca12c0cf88317e2c083bcb11
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-30 08:38:53 +0500

    Update header to OZAT Engineering Hub: gcloud-run-revisions-list.sh

--- gcloud-run-revisions-list.sh
+++ gcloud-run-revisions-list.sh
@@ -1,8 +1,7 @@
 # ==============================================================================
-# gcloud-run-revisions-list.sh
+# Спас прод из поезда "Тальго" Алматы-Астана: 10 команд Cloud Shell, которые спасут ваш DevOps без ноутбука
 # Source: OZAT Engineering Hub (https://ozat.kz)
 # GitHub: https://github.com/OZAT-kz/blog-commands/blob/main/gcloud-run-revisions-list.sh
 # ==============================================================================
-
 
 gcloud run revisions list --service=api-gateway
