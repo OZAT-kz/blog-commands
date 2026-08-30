7728192dfec3e904a7cc5c629ad82ad89db63b7d
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-30 08:34:04 +0500

    Update header to OZAT Engineering Hub: kubectl-get-pods.sh

--- kubectl-get-pods.sh
+++ kubectl-get-pods.sh
@@ -1,6 +1,6 @@
 # ==============================================================================
 # kubectl-get-pods.sh
-# Source: OZAT Engineering Blog (https://ozat.kz)
+# Source: OZAT Engineering Hub (https://ozat.kz)
 # GitHub: https://github.com/OZAT-kz/blog-commands/blob/main/kubectl-get-pods.sh
 # ==============================================================================
 
