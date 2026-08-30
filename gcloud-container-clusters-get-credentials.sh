a3345f2151d2be091c09ee3b30eb88f34dd55af4
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-30 08:38:50 +0500

    Update header to OZAT Engineering Hub: gcloud-container-clusters-get-credentials.sh

--- gcloud-container-clusters-get-credentials.sh
+++ gcloud-container-clusters-get-credentials.sh
@@ -1,8 +1,7 @@
 # ==============================================================================
-# gcloud-container-clusters-get-credentials.sh
+# Спас прод из поезда "Тальго" Алматы-Астана: 10 команд Cloud Shell, которые спасут ваш DevOps без ноутбука
 # Source: OZAT Engineering Hub (https://ozat.kz)
 # GitHub: https://github.com/OZAT-kz/blog-commands/blob/main/gcloud-container-clusters-get-credentials.sh
 # ==============================================================================
-
 
 gcloud container clusters get-credentials prod-cluster --region=europe-west3
